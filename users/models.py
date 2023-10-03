5817d695b749f4b109e13b4c92577a8ef458faa3
svro <ichistrawberry2012@gmail.com>
2023-10-03 13:35:26 +0300

    user model

--- users/models.py
+++ users/models.py
@@ -1,7 +1,20 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Constants
+NULLABLE = {'blank': True, 'null': True}
 
 
 # User
 
 class User(AbstractUser):
-    pass
+    '''Пользователь'''
+    username = models.CharField(unique=True, max_length=100, verbose_name="Никнейм в ТГ")
+    telegram_id = models.IntegerField(verbose_name="Телеграм ID", **NULLABLE)
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = 'Пользователи'
