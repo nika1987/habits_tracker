3c79e6f175cf9eb8dad8c6f10b2f3842542a185d
svro <ichistrawberry2012@gmail.com>
2023-10-04 15:29:17 +0300

    habits auth

--- habits/views.py
+++ habits/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from habits.permissions import IsOwner
 
 from habits.serializers import HabitSerializer, HabitCreateSerializer
 
@@ -13,29 +15,41 @@ from users.models import User
 class HabitCreateAPIView(generics.CreateAPIView):
     '''CREATE Habit'''
     serializer_class = HabitCreateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        new_habit = serializer.save()
+        new_habit.user = self.request.user
+        new_habit.save()
 
 
 class HabitListAPIView(generics.ListAPIView):
     '''READ ALL Habits'''
     serializer_class = HabitSerializer
-    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated & IsOwner]
+
+    def get_queryset(self):
+        return Habit.objects.filter(user=self.request.user)
 
 
 class HabitRetrieveAPIView(generics.RetrieveAPIView):
     '''READ ONE Habit'''
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated & IsOwner]
 
 
 class HabitUpdateAPIView(generics.UpdateAPIView):
     '''UPDATE PUT AND PATCH Habits'''
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated & IsOwner]
 
 
 class HabitDestroyAPIView(generics.DestroyAPIView):
     '''DELETE Habit'''
     queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated & IsOwner]
 
 
 
@@ -47,3 +61,4 @@ class PublicHabitListAPIView(generics.ListAPIView):
     '''READ ALL Public Habits'''
     serializer_class = HabitSerializer
     queryset = Habit.objects.filter(is_public=True)
+    permission_classes = [AllowAny]
